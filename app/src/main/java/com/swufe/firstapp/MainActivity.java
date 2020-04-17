9d5c0c2537f6486dc825557984ea5de690821079
Songxingchen <1208429213@qq.com>
2020-04-17 11:35:03 +0800

    score indicator for two teams

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -8,17 +8,52 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView out;
+    EditText inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        //add something....
+          TextView out = (TextView) findViewById(R.id.Showtext);
+          EditText inp = (EditText) findViewById(R.id.InpText);
+
+
+        Button btn = (Button)findViewById(R.id.btn);
+
+        //btn.setOnClickListener(new View.OnClickListener() {  //创建新的对象进行监听
+           // @Override
+            //public void onClick(View v) {
+                //Log.i("click","onClick1");
+                //String str = inp.getText().toString();
+                //int num = Integer.valueOf(str).intValue();
+                //out.setText("The temperature is:" + num*1.8+32);
+            //}
+        //});
+        btn.setOnClickListener(this);  //直接使用MainActivity对象监听
     }
 
+    @Override
+    public void onClick(View v) {
+        Log.i("click","onClick2");
+
+            //TextView tv = (TextView) findViewById(R.id.Showtext);
+
+            //EditText inp = (EditText) findViewById(R.id.InpText);
+        String str = inp.getText().toString();
+        int num = Integer.valueOf(str).intValue();
+        out.setText("The temperature is:" + num*1.8+32);
+    }
 }
---
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -0,0 +1,61 @@
+package com.swufe.firstapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    private static final String TAG ="second" ;
+    TextView score;
+    TextView score2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+        score=findViewById(R.id.score1);
+        score2=findViewById(R.id.score2);
+    }
+
+
+    public void btnAdd1(View v) {
+        if(v.getId()==R.id.button_4)
+            showScore(1);
+        else{
+            showScore2(1);
+        }
+    }
+    public void btnAdd2(View v) {
+        if(v.getId()==R.id.button_5)
+            showScore(2);
+        else{
+            showScore2(2);
+        }
+    }
+    public void btnAdd3(View v) {
+        if(v.getId()==R.id.button_6)
+            showScore(3);
+        else{
+            showScore2(3);
+        }
+    }
+    public void reset(View v) {
+        score.setText("0");
+        score2.setText("0");
+    }
+    public void showScore(int inc) {
+        Log.i("main","inc="+inc);
+        String oldScore=(String) score.getText();
+        int newScore=Integer.parseInt(oldScore)+inc;
+        score.setText(""+newScore);
+    }
+    public void showScore2(int inc) {
+        Log.i("main","inc="+inc);
+        String oldScore=(String) score2.getText();
+        int newScore=Integer.parseInt(oldScore)+inc;
+        score2.setText(""+newScore);
+    }
+}

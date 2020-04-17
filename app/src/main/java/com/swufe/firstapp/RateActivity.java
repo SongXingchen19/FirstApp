470c5cff170776f098e0064f986c0b0751ccf07a
Songxingchen <1208429213@qq.com>
2020-04-17 23:44:08 +0800

    money exchange app 06 理解多线程及Handler传值
    理解权限设置及网页数据获取

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +17,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RateActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RateActivity extends AppCompatActivity implements Runnable{
 
     private final String TAG = "rate";
     private float dollarRate = 0.1f;
@@ -25,6 +34,7 @@ public class RateActivity extends AppCompatActivity {
 
     EditText rmb;
     TextView show;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +53,20 @@ public class RateActivity extends AppCompatActivity {
         Log.i(TAG,"onCreate: sp euroRate" + euroRate);
         Log.i(TAG,"onCreate: sp wonRate" + wonRate);
 
+        //开启子线程
+        Thread t = new Thread(this);
+        t.start();
+        handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if (msg.what == 5){
+                    String str = (String) msg.obj;
+                    Log.i(TAG,"HandleMessage: getMessage msg ="+str);
+                    show.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
     }
 
     public void onClick(View btn){
@@ -131,8 +155,57 @@ public class RateActivity extends AppCompatActivity {
             editor.putFloat("euro_rate",euroRate);
             editor.putFloat("won_rate",wonRate);
             editor.commit();
-            Log.i(TAG,"onActivityResult已保存到sharedPreferences")；
+            Log.i(TAG,"onActivityResult已保存到sharedPreferences");
         }
         super.onActivityResult(requestCode,resultCode,data);
+    }
+
+    @Override
+    public void run() {
+        Log.i(TAG,"run:run()......");
+        for (int i = 1;i<6;i++){
+            Log.i(TAG,"run:i=" + i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //获取msg对象，用于返回主线程
+        Message msg = handler.obtainMessage(5);
+        //msg.what = 5;
+        msg.obj = "hello from run()";
+        handler.sendMessage(msg);
+
+
+        //获取网络数据
+        try {
+            URL url = new URL("http://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG,"run:html=" + html);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private String inputStream2String(InputStream InputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(InputStream,"gb2312");
+        for (;;){
+            int rsz = in.read(buffer,0,buffer.length);
+            if (rsz < 0){
+                break;
+            }
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }

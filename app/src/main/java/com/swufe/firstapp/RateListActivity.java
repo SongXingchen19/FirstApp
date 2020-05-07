f2c5cc4cc51a21ea7eeb177850e138497ab02443
Songxingchen <1208429213@qq.com>
2020-05-07 23:12:06 +0800

    RateListActivity 创建简单列表

--- app/src/main/java/com/swufe/firstapp/RateListActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateListActivity.java
@@ -2,13 +2,25 @@ package com.swufe.firstapp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RateListActivity extends ListActivity {
-    String date[] = {"one","two","three"};
+    String data[] = {"one","two","three"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_rate_list);        一定记得注释掉！！
+        List<String> list1 = new ArrayList<String>();
+        for (int i=1;i<100;i++){
+            list1.add("list" + i);
+        }
+
+        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        setListAdapter(adapter);
     }
 }

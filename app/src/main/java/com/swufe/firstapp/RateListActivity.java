2aaa092e9a20fabb92ddcedb3ee2c0e1d684f538
Songxingchen <1208429213@qq.com>
2020-06-04 21:28:19 +0800

    12在应用中使用SQLite数据库

--- app/src/main/java/com/swufe/firstapp/RateListActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateListActivity.java
@@ -1,8 +1,6 @@
 package com.swufe.firstapp;
 
 import android.app.ListActivity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -18,13 +16,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class RateListActivity extends ListActivity implements Runnable{
@@ -46,9 +38,9 @@ public class RateListActivity extends ListActivity implements Runnable{
         }
 
         //获取SP中的日期记录，放在super.onCreate(…)之后
-        SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+        /*SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
         logDate = sp.getString(DATE_SP_KEY, "");
-        Log.i("List","lastRateDateStr=" + logDate);
+        Log.i("List","lastRateDateStr=" + logDate);*/
 
 
         ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
@@ -72,70 +64,45 @@ public class RateListActivity extends ListActivity implements Runnable{
 
     @Override
     public void run() {
-        Log.i("List","run...");
+        //获取网络数据，放入list带回到主线程中
         List<String> retList = new ArrayList<String>();
-        Message msg = handler.obtainMessage();
-        String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
-        Log.i("run","curDateStr:" + curDateStr + " logDate:" + logDate);
-        if(curDateStr.equals(logDate)){
-            //如果相等，则不从网络中获取数据
-            Log.i("run","日期相等，从数据库中获取数据");
-            DBManager dbManager = new DBManager(RateListActivity.this);
-            for(RateItem rateItem : dbManager.listAll()){
-                retList.add(rateItem.getCurName() + "=>" + rateItem.getCurRate());
-            }
-        }else{
-            Log.i("run","日期相等，从网络中获取在线数据");
-            //获取网络数据
-            try {
-                List<RateItem> rateList = new ArrayList<RateItem>();
-                URL url = new URL("http://www.usd-cny.com/bankofchina.htm");
-                HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-                InputStream in = httpConn.getInputStream();
-                String retStr = IOUtils.toString(in,"gb2312");
+        Document doc = null;
+        try {
+            Thread.sleep(3000);
+            doc = Jsoup.connect("http://www.usd-cny.com/bankofchina.htm").get();
+            //doc = Jsoup.parse(html);
+            Log.i(TAG,"run:" + doc.title());
+            Elements tables = doc.getElementsByTag("table");
+            int i = 1;
+        /*for (Element table : tables){
+            Log.i(TAG, "run: table["+i+"]=" + table);
+            i++;
+        }*/
 
-                //Log.i("WWW","retStr:" + retStr);
-                //需要对获得的html字串进行解析，提取相应的汇率数据...
+            //从第几个table中获取数据
+            Element table6 = tables.get(5);
+            //Log.i(TAG, "run: table6=" + table6);
 
-                Document doc = Jsoup.parse(retStr);
-                Elements tables  = doc.getElementsByTag("table");
+            //获取TD中的数据
+            Elements tds = table6.getElementsByTag("td");
+            for (int I=0;I<tds.size();I+=6) {
+                Element td1 = tds.get(I);
+                Element td2 = tds.get(I+5);
+                Log.i(TAG, "run: " + td1.text() + "==>" + td2.text());
+                String str1 = td1.text();
+                String val = td2.text();
 
-                Element retTable = tables.get(5);
-                Elements tds = retTable.getElementsByTag("td");
-                int tdSize = tds.size();
-                for(int i=0;i<tdSize;i+=8){
-                    Element td1 = tds.get(i);
-                    Element td2 = tds.get(i+5);
-                    //Log.i("www","td:" + td1.text() + "->" + td2.text());
-                    float val = Float.parseFloat(td2.text());
-                    val = 100/val;
-                    retList.add(td1.text() + "->" + val);
-
-                    RateItem rateItem = new RateItem(td1.text(),td2.text());
-                    rateList.add(rateItem);
-                }
-                DBManager dbManager = new DBManager(RateListActivity.this);
-                dbManager.deleteAll();
-                Log.i("db","删除所有记录");
-                dbManager.addAll(rateList);
-                Log.i("db","添加新记录集");
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                Log.i(TAG, "run: "+ str1 + "==>" + val);
+                retList.add(str1 + "==>" + val);
             }
 
-            //更新记录日期
-            SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
-            SharedPreferences.Editor edit = sp.edit();
-            edit.putString(DATE_SP_KEY, curDateStr);
-            edit.commit();
-            Log.i("run","更新日期结束：" + curDateStr);
+
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
         }
 
+        Message msg = handler.obtainMessage(7);
         msg.obj = retList;
-        msg.what = msgWhat;
         handler.sendMessage(msg);
     }
 }

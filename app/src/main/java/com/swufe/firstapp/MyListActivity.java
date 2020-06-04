b30da55e76f28c1af09ab3cc323e653f3ea968ea
Songxingchen <1208429213@qq.com>
2020-06-04 16:24:20 +0800

    11_2.2列表数据的删除

--- app/src/main/java/com/swufe/firstapp/MyListActivity.java
+++ app/src/main/java/com/swufe/firstapp/MyListActivity.java
@@ -1,9 +1,12 @@
 package com.swufe.firstapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,10 +17,23 @@ public class MyListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
 
-        GridView listView = (GridView) findViewById(R.id.mylist);
+        ListView listView = (ListView) findViewById(R.id.mylist);
         String data[] = {"111","222"};
 
         ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
         listView.setAdapter(adapter);
+
+        listView.setEmptyView(findViewById(R.id.nodata));
+        listView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> listview, View view, int position, long id) {
+        Log.i(TAG, "onItemClick: position=" + position);
+        Log.i(TAG, "onItemClick: parent" + listview);
+        adapter.remove(listview.getItemAtPosition(position));
+        //adapter.notifyDataSetChanged();
     }
 }
